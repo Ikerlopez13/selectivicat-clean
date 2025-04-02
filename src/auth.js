685ddb4fe7953c96226635c1254faab8395f7d74
Iker.dev <lopezalegreiker@gmail.com>
2025-04-02 20:06:48 +0200

    fix: simplify auth configuration to use only Google provider

--- src/auth.js
+++ src/auth.js
@@ -8,6 +8,19 @@ const config = {
       clientSecret: process.env.GOOGLE_SECRET,
     }),
   ],
+  secret: process.env.AUTH_SECRET,
+  pages: {
+    signIn: "/",
+  },
+  callbacks: {
+    async redirect({ url, baseUrl }) {
+      // Allows relative callback URLs
+      if (url.startsWith("/")) return `${baseUrl}${url}`
+      // Allows callback URLs on the same origin
+      else if (new URL(url).origin === baseUrl) return url
+      return baseUrl
+    },
+  },
 };
 
 export const { handlers, signIn, signOut, auth } = NextAuth(config);
